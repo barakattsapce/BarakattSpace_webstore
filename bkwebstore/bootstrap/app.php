787001603ee8d1e655c4b57f521843904b26fb67
Bara Kat <barakattspace@email.com>
2026-05-22 08:01:31 -0700

    fix laravel 11 middleware structure

--- bkwebstore/bootstrap/app.php
+++ bkwebstore/bootstrap/app.php
@@ -11,11 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-
-        $middleware->statefulApi();
-
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-
+    ->withExceptions(function (Exceptions $exceptions) {
+       
     })->create();
